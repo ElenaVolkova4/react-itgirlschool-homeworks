4ea05faca6447dc1fe476e39648b555ab402e2b9
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-10-29 22:03:28 +0300

    Добавила title к графику week25

--- week25/week25app/src/App.js
+++ week25/week25app/src/App.js
@@ -9,31 +9,46 @@ function App() {
   return (
 
     <div className="App">
-    <h1>Сравнение фреймворков/библиотек JavaScript по состоянию на 01.10.2021 г. по данным GoogleTrends</h1>
+      {/* <h1>Сравнение фреймворков/библиотек JavaScript по состоянию на 01.10.2021 г. по данным GoogleTrends</h1> */}
 
-{/* <SketchPicker /> //выборцвета */}
+      {/* <SketchPicker /> //выборцвета */}
 
 
-<Pie data={
-{
-  labels: [
-    'React',
-    'Angular',
-    'Vue.js',
-  ],
-  datasets: [{
-    label: 'Сравнение фреймворков/библиотек JavaScript по состоянию на 01.10.2021 г. по данным GoogleTrends',
-    data: [83, 23, 2],
-    backgroundColor: [
-      'blue',
-      'red',
-      'green'
-    ],
-    hoverOffset: 4
-  }]
-}
-}
-/>      
+      <Pie data={
+        {
+          labels: [
+            'React',
+            'Angular',
+            'Vue.js',
+          ],
+          datasets: [{
+            label: 'Сравнение фреймворков/библиотек JavaScript по состоянию на 01.10.2021 г. по данным GoogleTrends',
+            data: [83, 23, 2],
+            backgroundColor: [
+              'blue',
+              'red',
+              'green'
+            ],
+            hoverOffset: 4
+          }]
+        }
+      }
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: ['Сравнение фреймворков/библиотек JavaScript по состоянию на 01.10.2021 г. по данным GoogleTrends'],
+              color: 'pink',
+              fullSize: false,
+              // fontSize: '190',
+              font: {
+                size: 70
+              },
+              position: 'top',
+            }
+          }
+        }}
+      />
     </div>
   );
 }
