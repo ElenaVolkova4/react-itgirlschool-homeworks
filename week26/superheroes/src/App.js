0d1d7328f3c773a2508220951d4a1ca7afa5564f
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-11-04 10:30:59 +0300

    созд new приложение superheroes, почти вся теория

---
+++ week26/superheroes/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/card';
+
+
+
+
+
+
+function App(props) {
+  return (
+    <div className="App">
+
+<Card heroName="z"/>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
