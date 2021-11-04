2655e5ee291f40335bd70d46aefe39cc6cb5943d
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-11-04 23:38:15 +0300

    попробовала сделать базу карточек

--- languageapp/src/App.js
+++ languageapp/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { words } from './assets/components/dataWords'
+
+
+
 function App() {
+
+  console.log(words)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
