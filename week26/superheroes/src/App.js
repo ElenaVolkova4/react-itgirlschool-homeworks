41020875de8d39529bfb1b16c17e0b294826a900
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-11-06 23:47:20 +0300

    Попровила дз week26

--- week26/superheroes/src/App.js
+++ week26/superheroes/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import CardHero from './components/card';
+import CardHero from './components/CardHero';
 import { heroes } from './components/dataHeroes'
 
 function App() {
@@ -9,7 +9,9 @@ function App() {
       <h1>Супергерои современности</h1>
       {
         heroes.map((hero) =>//переменную hero мы назвали сами
-          <CardHero heroName={hero.heroName} pic={hero.pic} alterEgo={hero.alterEgo} universe={hero.universe} superPower={hero.superPower} details={hero.details} />)
+          <CardHero
+            key={hero.id}//надо обязательно задавать ключ
+            heroName={hero.heroName} pic={hero.pic} alterEgo={hero.alterEgo} universe={hero.universe} superPower={hero.superPower} details={hero.details} />)
       }
     </div>
   );
