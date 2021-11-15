7826dc496ed33f212f5123263acca9f4cd77c800
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-11-15 14:17:04 +0300

    переименование Header

--- languageapp/src/assets/components/Header.js
+++ languageapp/src/assets/components/Header.js
@@ -1,13 +1,11 @@
 import "./Header.scss";
-// import logo from "../images/logo5.jpg"
 import logo from "../images/logo10.png"
 
 
 const Header = () => {
   return <div className="header">
-    <img className="logo" src={logo} alt="Логотип"/>
+    <img className="logo" src={logo} alt="Логотип приложения"/>
     <p>Чтобы попасть в Хогвардс - надо знать английский язык!</p>
-    <div>Выбрать категорию</div>
     <button>Добавить слово</button>
   </div>;
  }
