98cade0de1b587d0cfed748b052cae8f942ebf9c
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-11-15 13:50:37 +0300

    переименовала Footer

--- languageapp/src/assets/components/Footer.jsx
+++ languageapp/src/assets/components/Footer.jsx
@@ -3,8 +3,8 @@ import "./Footer.scss";
 
 const Footer = () => {
   return <div className="footer">
-    <p class="foter__copyright">&#169; 2021</p>
-    <p class="footer__text">Волкова Елена</p>
+    <p class="foter_copyright">&#169; 2021</p>
+    <p class="footer_text">Волкова Елена</p>
 
   </div>;
  }
