802353b62bbd5f76770075a5a3be7b6187a3c455
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-11-26 21:49:32 +0300

    доделала 1п (вывела массив и 1сообщ зеленым)

--- week29/commentsapp/src/App.js
+++ week29/commentsapp/src/App.js
@@ -6,41 +6,28 @@ function App() {
   const [newComment, setNewComment] = useState(""); //новый комментарий в инпуте
   const [addedComment, setComment] = useState([]); //массив, куда должны попадать сообщения, введенные в InputComment
 
-  //от Зои
-  //наведение красоты массива комментов
-  const saveFunction = () => {
-    const finalMessage = value.replace(/ +/g, " ").trim();
-    if (finalMessage) {
-      setArray([finalMessage.replace(/(\r\n|\n|\r)/gm, ""), ...array]);
-      console.log(array);
-    } else {
-      setValue("");
-      return;
-    }
-    setValue("");
-  };
-
   const handleClick = () => {
     //ф-ция, которая добавляет value inputа в массив комментов и очищает инпут
 
-    const commentsCopy = [...addedComment]; //массив с комментами
-    console.log(commentsCopy);
+    const commentsCopy = [...addedComment]; //создается массив с комментами
+    // console.log(commentsCopy);
     commentsCopy.push(newComment); //добавление нового коммента в массив
-    setComment(commentsCopy);
-    setNewComment(""); //очищение инпута
-    // {
-    //   for (let i = 0; i < addedComment.length; i++) {
-    //     document.querySelector(".App_comments").innerHTML = addedComment[i] + <br>;
-    //   }
-    // }
-    // let comments = addedComment.map(function (item) {
-    //   return <span>{item}</span>;
-    // });
+    setComment(commentsCopy); //перезаписывание массива
+    setNewComment(""); //очищение поля инпута
   };
   return (
     <div className="App">
-      <div className="App_comments">{addedComment}</div>
-      <input
+      <div className="App_comments">
+        {addedComment.map((message, i) => {
+          return (
+            //с условием, что первое сообщение зеленым цветом
+            <div key={i} className={i === 0 ? "green" : ""}>
+              {message}
+            </div>
+          );
+        })}
+      </div>
+      <textarea
         id="comment"
         value={newComment}
         onChange={(e) => setNewComment(e.target.value)}
