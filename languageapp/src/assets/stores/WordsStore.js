55569fed72d803c2a84148253a5b9e48853be2df
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-12-30 20:20:45 +0300

    попытка создать стор

--- languageapp/src/assets/stores/WordsStore.js
+++ languageapp/src/assets/stores/WordsStore.js
@@ -1,37 +1,36 @@
-import { action, observable } from 'mobx';
+import { makeAutoObservable, action, observable } from 'mobx';
 
 class WordsStore {
   words = [];
+  constructor() {
+    makeAutoObservable(this);
+  }
 
   loadWords = () => {
     fetch('/api/words')
-    .then(response => response.json())
-    .then(words => {
-      setWords(words);
-      // setisWordsLoading(false);
-    })
-    .catch(error => {
-      console.log(error);
-      // setisWordsLoading(false);
-      // setError(true);
-    });
-  }
-    addWord = word => {
+      .then(response => response.json())
+      .then(words => {
+        setWords(words);
+        // setisWordsLoading(false);
+      })
+      .catch(error => {
+        console.log(error);
+        // setisWordsLoading(false);
+        // setError(true);
+      });
+  };
+  addWord = word => {
     return this.words.push(word);
   };
 
-    removeWord = index => {
+  removeWord = index => {
     return this.words.splice(index, 1);
   };
-
-    useEffect(() => {
-    loadWords();
-  }, []);
 }
-decorate(WordsStore, {
-  words: observable,
-  addWord: action,
-  removeWord: action,
-});
+// decorate(WordsStore, {
+//   words: observable,
+//   addWord: action,
+//   removeWord: action,
+// });
 
 export default WordsStore;
