a20e7d8e09cfa467618b9343fa554433e546d75c
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2022-01-06 23:03:05 +0300

    удаление работает

--- languageapp/src/assets/stores/WordsStore.js
+++ languageapp/src/assets/stores/WordsStore.js
@@ -49,7 +49,24 @@ class WordsStore {
   };
 
   removeWord = id => {
-    this.words = this.words.filter(el => el.id == !id);
+    this.words = this.words.filter(el => el.id !== id);
+    fetch(`/api/words/${id}/delete`, {
+      method: 'POST', //по умолчанию используется GET, поэтому POST надо конкретно прописать
+      body: JSON.stringify(this.words),
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8', //отправляем в формате JSON
+      },
+    })
+      .then(response => response.json())
+      .then(word => {
+        console.log(word);
+        // updateData();
+      })
+      .catch(error => {
+        console.log(error);
+        this.isLoading = false;
+        this.serverError = true;
+      });
   };
 }
 export default WordsStore;
